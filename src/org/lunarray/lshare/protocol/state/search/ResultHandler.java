3bc61a8aad30dc1e0a150f6b18a53f29e3643a64
kerthale <kerthale@78a92dc1-440f-0410-ab52-efb9a00e3c90>
2006-04-06 20:35:28 +0000

    Started work on Documenting.
    done:
    org.lunarray.lshare
    org.lunarray.lshare.gui
    org.lunarray.lshare.gui.contactlist
    org.lunarray.lshare.gui.filelist
    org.lunarray.lshare.gui.main
    org.lunarray.lshare.gui.search
    org.lunarray.lshare.gui.sharelist
    org.lunarray.lshare.protocol
    org.lunarray.lshare.protocol.events
    org.lunarray.lshare.protocol.filelist
    org.lunarray.lshare.protocol.packets
    org.lunarray.lshare.protocol.packets.search
    org.lunarray.lshare.protocol.packets.user
    org.lunarray.lshare.protocol.settings
    org.lunarray.lshare.protocol.state.search
    
    git-svn-id: file:///home/kerthale/projects/../projects/lshare@40 78a92dc1-440f-0410-ab52-efb9a00e3c90

--- src/org/lunarray/lshare/protocol/state/search/ResultHandler.java
+++ src/org/lunarray/lshare/protocol/state/search/ResultHandler.java
@@ -3,16 +3,30 @@ package org.lunarray.lshare.protocol.state.search;
 import org.lunarray.lshare.protocol.Controls;
 import org.lunarray.lshare.protocol.tasks.RunnableTask;
 
+/**
+ * A handler for a search result.
+ * @author Pal Hargitai
+ */
 public class ResultHandler implements RunnableTask {
 
+	/**
+	 * The result to handle.
+	 */
 	private SearchResult result;
 	
+	/**
+	 * Constructs a result handler.
+	 * @param r The result to handle.
+	 */
 	public ResultHandler(SearchResult r) {
 		result = r;
 	}
 	
+	/**
+	 * Processes the search results.
+	 * @param c The controls to the protocol.
+	 */
 	public void runTask(Controls c) {
 		c.getState().getSearchList().processResult(result);
 	}
-
 }
--- src/org/lunarray/lshare/protocol/state/search/SearchList.java
+++ src/org/lunarray/lshare/protocol/state/search/SearchList.java
@@ -8,24 +8,51 @@ import org.lunarray.lshare.protocol.events.SearchListener;
 import org.lunarray.lshare.protocol.packets.search.SearchOut;
 import org.lunarray.lshare.protocol.state.userlist.User;
 
+/**
+ * The search list for handling searching.
+ * @author Pal Hargitai
+ */
 public class SearchList implements ExternalSearchList {
 
+	/**
+	 * The registered listeners.
+	 */
 	private ArrayList<SearchListener> listeners;
+	
+	/**
+	 * The controls for the rest of the protocol.
+	 */
 	private Controls controls;
 	
+	/**
+	 * Constructs the search list.
+	 * @param c The controls to the protocol.
+	 */
 	public SearchList(Controls c) {
 		listeners = new ArrayList<SearchListener>();
 		controls = c;
 	}
 
+	/**
+	 * Registeres a listener for search results.
+	 * @param lis The listener to register.
+	 */
 	public void addListener(SearchListener lis) {
 		listeners.add(lis);
 	}
 	
+	/**
+	 * Unregisteres a listener for search results.
+	 * @param lis The listener to unregister.
+	 */
 	public void removeListener(SearchListener lis) {
 		listeners.remove(lis);
 	}
 	
+	/**
+	 * Process a search result.
+	 * @param e A single search result to handle.
+	 */
 	public void processResult(SearchResult e) {
 		Controls.getLogger().fine("Result received: " + e.getName());
 		Controls.getLogger().fine("Form: " + e.getAddress().getHostName());
@@ -41,11 +68,19 @@ public class SearchList implements ExternalSearchList {
 		}
 	}
 	
+	/**
+	 * Search for a specific search string.
+	 * @param s The search string to search for.
+	 */
 	public void searchForString(String s) {
 		SearchOut so = new SearchOut(s);
 		controls.getUDPTransport().send(so);
 	}
 	
+	/**
+	 * Search for a specific file hash.
+	 * @param h The hash to search for.
+	 */
 	public void searchForHash(byte[] h) {
 		// TODO
 	}
